9d43b952ef3880f6239124480be13e462cbad205
Jason Pease <jasoncpease@gmail.com>
2014-04-14 20:06:19 -0400

    spiceRack Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpiceRack::Application.config.secret_key_base = '0df4a5ed93b226b91c4097a79446ca6388582f5d4a4cc540aa5217ef0b99c9c9771e5536eea3c668ceb38858a7b94462b4557adfe9d993bc447580ae04a8f482'
